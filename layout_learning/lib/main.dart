c4bda04a71074e5f0522bdb13a2b173d1f524416
bunny3025 <smarty.ad4@gmail.com>
2020-10-02 23:50:52 +0530

    adding floating button

--- layout_learning/lib/main.dart
+++ layout_learning/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:layout_learning/bunny/samples/box_layout.dart';
 import 'package:layout_learning/bunny/samples/hello.dart';
+import 'package:layout_learning/bunny/samples/tutorial_home.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Layout learning",
-    home: BoxLayoutScaffold(),
+    home: TutorialHome(),
   ));
 }
