2c44b912ac717d26a5877b52e70ec7ffca20adbf
bunny3025 <smarty.ad4@gmail.com>
2020-10-02 23:30:23 +0530

    added box containers, and added different widget like icons, search button etc. One thing is always return Material from stateless widget and get MaterialApp for run.

--- layout_learning/lib/bunny/samples/box_layout.dart
+++ layout_learning/lib/bunny/samples/box_layout.dart
@@ -8,6 +8,50 @@ class BoxLayout extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Container() ;
+    return Container(
+      height: 60,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "Navigation Menu",
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: "Search",
+            onPressed: null,
+          )
+        ],
+      ),
+    ) ;
+  }
+}
+
+class BoxLayoutScaffold extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: <Widget>[
+          BoxLayout(
+            title: Text(
+                "Example Title",
+                style: Theme.of(context).primaryTextTheme.headline5,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text("Hello World"),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- layout_learning/lib/main.dart
+++ layout_learning/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:layout_learning/bunny/samples/box_layout.dart';
 import 'package:layout_learning/bunny/samples/hello.dart';
 
 void main() {
-  runApp(HelloApp());
+  runApp(MaterialApp(
+    title: "Layout learning",
+    home: BoxLayoutScaffold(),
+  ));
 }
