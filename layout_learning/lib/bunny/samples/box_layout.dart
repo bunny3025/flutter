271853ee08c2816a5ae7741f3cb0bf979d203cfe
bunny3025 <smarty.ad4@gmail.com>
2020-10-02 22:12:31 +0530

    Added Box layout and with blank container

---
+++ layout_learning/lib/bunny/samples/box_layout.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BoxLayout extends StatelessWidget{
+
+  BoxLayout({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container() ;
+  }
+}
