2f508741f0565104a6898771c71878dc32779694
bunny3025 <smarty.ad4@gmail.com>
2020-10-24 23:33:12 +0530

    add dynamic answers and their text

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(PersonalityQuestionApp());
 
@@ -13,7 +14,7 @@ class PersonalityQuestionApp extends StatefulWidget {
 class _PersonalityQuestionAppState extends State<PersonalityQuestionApp> {
   var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -22,13 +23,41 @@ class _PersonalityQuestionAppState extends State<PersonalityQuestionApp> {
   }
 
   var questions = [
-    'tipi tipi top which color you want?',
-    'seriously you wanted red color?',
-    'seriously you wanted blue color?',
-    'seriously you wanted red color?',
-    'seriously you wanted green color?',
-    'seriously you wanted yellow color?',
-    'seriously you wanted pink color?',
+    {
+      'questionText': 'tipi tipi top which color you want?',
+      'answers': [1, 2, 3, 4]
+    },
+    {
+      'questionText': 'seriously you wanted red color?',
+      'answers': [1, 2, 3, 4, 5]
+    },
+    {
+      'questionText': 'seriously you wanted yellow color?',
+      'answers': [1, 2, 3, 4, 5, 6]
+    },
+    {
+      'questionText': 'seriously you wanted green color?',
+      'answers': [1, 2, 3, 4, 5],
+    },
+    {
+      'questionText': 'seriously you wanted red color?',
+      'answers': [
+        1,
+        2,
+        3,
+      ]
+    },
+    {
+      'questionText': 'seriously you wanted blue color?',
+      'answers': [
+        1,
+        2,
+      ]
+    },
+    {
+      'questionText': 'seriously you wanted pink color?',
+      'answers': [1, 2, 3, 4]
+    },
   ];
 
   @override
@@ -40,21 +69,11 @@ class _PersonalityQuestionAppState extends State<PersonalityQuestionApp> {
             ),
             body: Column(
               children: [
-                Question(questions[_questionIndex]),
-                RaisedButton(
-                  child: Text('Answer 1'),
-                  onPressed: answerQuestion,
-                ),
-                RaisedButton(
-                  child: Text('Answer 2'),
-                  onPressed: answerQuestion,
-                ),
-                RaisedButton(
-                  child: Text('Answer 3'),
-                  onPressed: () {
-                    print('answer 3 choosen');
-                  },
-                ),
+                Question(questions[_questionIndex]['questionText']),
+                ...(questions[_questionIndex]['answers'] as List<int>)
+                    .map((answer) {
+                  return Answer(_answerQuestion, answer.toString());
+                }).toList()
               ],
             )));
   }
