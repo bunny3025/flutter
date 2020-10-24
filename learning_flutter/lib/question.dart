8ac46170e4d3ef912c43e719d1c7f261587c9ddd
bunny3025 <smarty.ad4@gmail.com>
2020-10-24 22:55:56 +0530

    add some styling, touch up!!!

--- learning_flutter/lib/question.dart
+++ learning_flutter/lib/question.dart
@@ -7,6 +7,15 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      color: Colors.blueGrey,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
