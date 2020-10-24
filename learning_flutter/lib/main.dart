8ea6a1b69981ce86c19c17727b3c779e27568d1c
bunny3025 <smarty.ad4@gmail.com>
2020-10-24 19:54:12 +0530

    how to create StatelessWidget, and play with Array with button events

---
+++ learning_flutter/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PersonalityQuestionApp());
+
+class PersonalityQuestionApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+    print(questions.elementAt(questionIndex));
+  }
+
+  var questions = [
+    'tipi tipi top which color you want?',
+    'seriously you wanted red color?',
+    'seriously you wanted blue color?',
+    'seriously you wanted red color?',
+    'seriously you wanted green color?',
+    'seriously you wanted yellow color?',
+    'seriously you wanted pink color?',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(questions.elementAt(questionIndex)),
+            ),
+            body: Column(
+              children: [
+                Text(questions.elementAt(0)),
+                RaisedButton(
+                  child: Text('Answer 1'),
+                  onPressed: answerQuestion,
+                ),
+                RaisedButton(
+                  child: Text('Answer 2'),
+                  onPressed: answerQuestion,
+                ),
+                RaisedButton(
+                  child: Text('Answer 3'),
+                  onPressed: () {
+                    print('answer 3 choosen');
+                  },
+                ),
+              ],
+            )));
+  }
+}
