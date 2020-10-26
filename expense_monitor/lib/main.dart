93f0e69fa0edce103b32f992f0a7d29e9ae9ad32
bunny3025 <smarty.ad4@gmail.com>
2020-10-26 10:27:10 +0530

    Played with Card widget and Column widget for position of the widget on the screen.

--- expense_monitor/lib/main.dart
+++ expense_monitor/lib/main.dart
@@ -19,8 +19,28 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Expense Monitor'),
       ),
-      body: Center(
-        child: Text('Expense Monitor Playground !!!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Container(
+                color: Colors.blue,
+                child: Text(
+                  'Chart placeholder',
+                ),
+                //width: double.infinity,
+              ),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('all the tx avail here'),
+            color: Colors.red,
+          )
+        ],
       ),
     );
   }
