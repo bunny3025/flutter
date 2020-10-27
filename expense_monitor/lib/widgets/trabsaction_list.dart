baf795d28a5ab2af0313428a27c093e5ea85df24
bunny3025 <smarty.ad4@gmail.com>
2020-10-27 08:35:00 +0530

    #card-48055018: ListView Builder, should be used when list contents are more and only wanted to create widget over the visible screen. This way we can optimize our memory objects which are being created.

--- expense_monitor/lib/widgets/trabsaction_list.dart
+++ expense_monitor/lib/widgets/trabsaction_list.dart
@@ -11,8 +11,8 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView(
-        children: transaction.map((tx) {
+      child: ListView.builder(
+        itemBuilder: (bCtx, index) {
           return Card(
             child: Row(
               children: <Widget>[
@@ -29,7 +29,7 @@ class TransactionList extends StatelessWidget {
                     vertical: 10,
                   ),
                   child: Text(
-                    '\u20B9 ${tx.amount}',
+                    '\u20B9 ${transaction[index].amount}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
@@ -44,7 +44,7 @@ class TransactionList extends StatelessWidget {
                     Container(
                       padding: EdgeInsets.all(5),
                       child: Text(
-                        tx.title,
+                        transaction[index].title,
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 16,
@@ -54,7 +54,7 @@ class TransactionList extends StatelessWidget {
                     Container(
                       padding: EdgeInsets.all(5),
                       child: Text(
-                        DateFormat.yMMMMd().format(tx.date),
+                        DateFormat.yMMMMd().format(transaction[index].date),
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 12,
@@ -67,7 +67,8 @@ class TransactionList extends StatelessWidget {
               ],
             ),
           );
-        }).toList(),
+        },
+        itemCount: transaction.length,
       ),
     );
   }
